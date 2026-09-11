093661f03b084bb7b6799c666336e90bb1ec3662
unknown <srid57117@gmail.com>
2026-09-11 16:19:06 +0530

    Added JavaScript day2

---
+++ js/day2/script.js
@@ -0,0 +1,39 @@
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+for(let i=1; i<=20; i++){
+
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+for(let i=1; i<=10; i++){
+    console.log(i*5);
+}
+
+for (let i = 1; i <= 5; i++) {
+    let pattern = "";
+
+    for (let j = 1; j <= i; j++) {
+        pattern += "* ";
+    }
+
+    console.log(pattern);
+}
+for (let i = 1; i <= 5; i++) {
+    let result = "";
+
+    for (let j = 1; j <= i; j++) {
+        result += j + " ";
+    }
+
+    console.log(result);
+}
+
+
+
